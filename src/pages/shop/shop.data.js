855fe0054725c1068d3a01ea19164b7de858d2a6
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-04-01 10:10:45 +0200

    Created the collection-item and preview components

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,243 @@
+const SHOP_DATA = [
+    {
+    id: 1,
+    title: 'VIRTUAL REALITY',
+    routeName: 'virtual-reality',
+    items: [
+      {
+        id: 1,
+        name: 'VR Glasses 1',
+        imageUrl: '',
+        price: 20
+      },
+      {
+        id: 2,
+        name: 'Cardboar Black',
+        imageUrl: '',
+        price: 15
+      },
+      {
+        id: 3,
+        name: 'Foldable VR Glasses',
+        imageUrl: '',
+        price: 25
+      },
+      {
+        id: 4,
+        name: 'VR Start Pro',
+        imageUrl: '',
+        price: 22
+      },
+      {
+        id: 5,
+        name: 'Google Cardboard 3D',
+        imageUrl: '',
+        price: 7
+      },
+      {
+        id: 6,
+        name: 'VR Glasses 2',
+        imageUrl: '',
+        price: 14
+      },
+      {
+        id: 7,
+        name: '3D Cardboard 2',
+        imageUrl: '',
+        price: 18
+      },
+      {
+        id: 8,
+        name: 'Google Cradboard simple',
+        imageUrl: '',
+        price: 14
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'GREEN TECHNOLOGIES',
+    routeName: 'green-technologies',
+    items: [
+      {
+        id: 1,
+        name: 'Wind Turbine Pro',
+        imageUrl: '',
+        price: 220
+      },
+      {
+        id: 2,
+        name: 'Solar Panel 1',
+        imageUrl: '',
+        price: 280
+      },
+      {
+        id: 3,
+        name: 'Solar Panels 2',
+        imageUrl: '',
+        price: 110
+      },
+      {
+        id: 4,
+        name: 'Wind Turbine III',
+        imageUrl: '',
+        price: 160
+      },
+      {
+        id: 5,
+        name: 'Solar Panel 5',
+        imageUrl: '',
+        price: 160
+      },
+      {
+        id: 6,
+        name: 'Pro Solar Panel 5 Units',
+        imageUrl: '',
+        price: 160
+      },
+      {
+        id: 7,
+        name: '2 Solar Panels',
+        imageUrl: '',
+        price: 190
+      },
+      {
+        id: 8,
+        name: 'Wind Turbine 6',
+        imageUrl: '',
+        price: 200
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'SMART HOME',
+    routeName: 'smart-home',
+    items: [
+      {
+        id: 1,
+        name: 'IP Camera Pro',
+        imageUrl: '',
+        price: 125
+      },
+      {
+        id: 2,
+        name: 'IP Camera 2',
+        imageUrl: '',
+        price: 90
+      },
+      {
+        id: 3,
+        name: 'IP Camera Full Set',
+        imageUrl: '',
+        price: 90
+      },
+      {
+        id: 4,
+        name: 'IP Camera 6',
+        imageUrl: '',
+        price: 165
+      },
+      {
+        id: 5,
+        name: 'IP Camera 10',
+        imageUrl: '',
+        price: 185
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'LAPTOPS',
+    routeName: 'laptops',
+    items: [
+      {
+        id: 1,
+        name: 'Laptop Model 1',
+        imageUrl: '',
+        price: 25
+      },
+      {
+        id: 2,
+        name: 'Laptop Model 2',
+        imageUrl: '',
+        price: 20
+      },
+      {
+        id: 3,
+        name: 'Laptop Model 3',
+        imageUrl: '',
+        price: 80
+      },
+      {
+        id: 4,
+        name: 'Laptop Model 4',
+        imageUrl: '',
+        price: 80
+      },
+      {
+        id: 5,
+        name: 'Laptop Model 5',
+        imageUrl: '',
+        price: 45
+      },
+      {
+        id: 6,
+        name: 'Laptop Model 6',
+        imageUrl: '',
+        price: 135
+      },
+      {
+        id: 7,
+        name: 'Laptop Model 7',
+        imageUrl: '',
+        price: 20
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: 'DRONES',
+    routeName: 'drones',
+    items: [
+      {
+        id: 1,
+        name: 'Drone 1',
+        imageUrl: '',
+        price: 325
+      },
+      {
+        id: 2,
+        name: 'Drone 2',
+        imageUrl: '',
+        price: 20
+      },
+      {
+        id: 3,
+        name: 'Drone 3',
+        imageUrl: '',
+        price: 25
+      },
+      {
+        id: 4,
+        name: 'Drone 4',
+        imageUrl: '',
+        price: 25
+      },
+      {
+        id: 5,
+        name: 'Drone 5',
+        imageUrl: '',
+        price: 40
+      },
+      {
+        id: 6,
+        name: 'Drone 6',
+        imageUrl: '',
+        price: 25
+      }
+    ]
+  }
+]
+
+export default SHOP_DATA;
