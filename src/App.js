7aa43047db5b91f05a8ff8ed65687bfecf1b1614
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-04-20 16:34:45 +0200

    created the checkout page and route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInUp from './pages/sign-in-up/sign-in-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import { setCurrentUser } from './redux/user/user.actions';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { selectCurrentUser } from './redux/user/user.selector';
@@ -46,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInUp />)} />
         </Switch>
       </div>
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart-selectors';
 import './cart-dropdown.styles.scss';
 import { FaCartArrowDown } from 'react-icons/fa';
+import { GoAlert } from 'react-icons/go';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
-        cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+        cartItems.length ?
+        cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+        ))
+        :
+          <span className="empty-message"><GoAlert className="empty-cart-icon" /> Your Cart Is Empty</span>
       }
     </div>
-      <CustomButton><FaCartArrowDown className="checkout-icon"/> GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={() => history.push('/checkout')}><FaCartArrowDown className="checkout-icon"/> GO TO CHECKOUT</CustomButton>
   </div>
 )
 
@@ -22,4 +29,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
