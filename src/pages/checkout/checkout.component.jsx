2916d32b3b4540280d8445d56cdca5eaf0f3964d
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-04-21 21:09:40 +0200

    Developed further the checkout page having a total

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selectors';
 import './checkout.styles.scss';
+import { FaBoxOpen, FaClipboardList, FaListOl, FaMoneyCheckAlt, FaTrashAlt } from 'react-icons/fa';
 
-const CheckoutPage = () => (
-  <div>
-    CHECKOUT PAGE
+const CheckoutPage = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span><FaBoxOpen className="cheackout-header-icon"/> PRODUCT</span>
+      </div>
+      <div className="header-block">
+        <span><FaClipboardList className="cheackout-header-icon"/> DESCRIPTION</span>
+      </div>
+      <div className="header-block">
+        <span><FaListOl className="cheackout-header-icon"/> QUANTITY</span>
+      </div>
+      <div className="header-block">
+        <span><FaMoneyCheckAlt className="cheackout-header-icon"/> PRICE</span>
+      </div>
+      <div className="header-block">
+        <span><FaTrashAlt className="cheackout-header-icon"/> REMOVE</span>
+      </div>
+    </div>
+    {
+      cartItems.map(cartItem =>
+        cartItem.name
+      )
+    }
+    <div className="total">
+  <span>TOTAL: ${total}</span>
+    </div>
   </div>
-)
+);
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
