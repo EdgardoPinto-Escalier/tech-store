854ff9eae45e09fd063b8586b86bee3ddd8884d6
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-04-25 11:25:28 +0200

    Added addItem and removeItem functionality to the checkout component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
+import { FaMinus, FaPlus, FaTrashAlt } from 'react-icons/fa';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const {name, imageUrl, price, quantity } = cartItem;
   return(
   <div className="checkout-item">
@@ -11,14 +12,20 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
       <img src={imageUrl} alt="item" />
     </div>
     <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
+      <span className="quantity">
+        <FaMinus className="checkout-minus-icon" onClick={() => removeItem(cartItem)} /> 
+          {quantity} 
+        <FaPlus className="checkout-plus-icon" onClick={() => addItem(cartItem)}/>
+      </span>
+    <span className="price">$ {price}.00</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}><FaTrashAlt /></div>
   </div>
 )};
 
 const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item))
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
